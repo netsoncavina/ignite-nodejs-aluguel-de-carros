18e8f478fa1203a6c5b71bb42c9b37647caa962a
Netson Cavina <netsoncavina@gmail.com>
2023-02-04 12:07:36 -0300

    refatoração no post de specification

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,17 +1,12 @@
 import { Router, request, response } from "express";
 import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
-import { CreateSpecificationService } from "../modules/useCases/createSpecification/CreateSpecificationUseCase";
+import { createSpecificationController } from "../modules/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 const specificationsRepository = new SpecificationsRepository();
 
 specificationsRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository
-  );
-  createSpecificationService.execute({ name, description });
-  return response.status(201).send();
+  return createSpecificationController.handle(request, response);
 });
 
 specificationsRoutes.get("/", (request, response) => {
