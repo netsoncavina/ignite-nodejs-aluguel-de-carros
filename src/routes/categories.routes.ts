cd524f2f6f91f51e58ce91306c49e6a87adb32bb
Netson Cavina <netsoncavina@gmail.com>
2023-02-04 11:33:35 -0300

    refatoração

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,18 @@
 import { Router } from "express";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { CreateCategoryUseCase } from "../modules/useCases/createCategory/CreateCategoryUseCase";
+import { createCategoryController } from "../modules/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (request, response) => {});
+categoriesRoutes.post("/", (request, response) => {
+  return createCategoryController.handle(request, response);
+});
 
 categoriesRoutes.get("/", (request, response) => {
   const all = categoriesRepository.list();
   return response.json(all);
-});
-
-categoriesRoutes.get("/:name", (request, response) => {
-  const { name } = request.params;
-  const category = categoriesRepository.findByName(name);
-  return response.json(category);
 });
 
 export { categoriesRoutes };
